5d9d15fa116a7b81a9c7f1ffcad248a73a134641
Dmitry Morozov <dimemorozovy@yandex.ru>
2018-02-12 12:51:46 +0300

    add new components from chat, new style

--- src/views/chat/index.js
+++ src/views/chat/index.js
@@ -1,16 +1,34 @@
 // @flow
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { Siderbar } from "./components/sidebar/";
+import { IncomingMessage } from "./components/incoming-message/";
+import { OutgoingMessage } from "./components/outgoing-message/";
 import "./style.scss";
 
-export class Chat extends Component {
+export class Chat extends PureComponent {
     render() {
         return (
             <div className="chat-page">
                 <div className="sidebar-wrapper">
                     <Siderbar />
                 </div>
-                <div className="chat-wrapper" />
+                <div className="chat-wrapper">
+                    <div className="chat-messages">
+                        <IncomingMessage />
+                        <OutgoingMessage />
+                        <IncomingMessage />
+                        <IncomingMessage />
+                        <OutgoingMessage />
+                    </div>
+                    <div className="chat-field">
+                        <input
+                            className="chat-input"
+                            type="text"
+                            name="message"
+                        />
+                        <button className="chat-button">Send</button>
+                    </div>
+                </div>
             </div>
         );
     }
